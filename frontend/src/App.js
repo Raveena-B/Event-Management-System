fc3d4033fd13d5676cf1e337f7f4b9a1ec388982
Raveena Bandara <raveenabandara801@gmail.com>
2022-05-20 18:27:17 +0530

    Create the project Folder Structure

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <main>
+          <Routes>
+            <Route></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
